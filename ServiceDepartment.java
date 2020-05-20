4278dafc80a71925bbb4b0ad4cea374400943549
Rusman313 <62064583+Rusman313@users.noreply.github.com>
2020-05-20 10:39:24 -0400

    added a service dept

---
+++ ServiceDepartment.java
@@ -0,0 +1,17 @@
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+
+public class ServiceDepartment extends Department {
+    public ServiceDepartment() {
+        super("Service", 20);
+    }
+
+    @Override
+    public boolean isOpenToday(LocalDateTime day) {
+        if (day.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
