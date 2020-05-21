c5d72e71c354890f9ec06bf96b871ea76cab46c9
Rusman313 <62064583+Rusman313@users.noreply.github.com>
2020-05-21 08:07:51 -0400

    added Discountable interface

---
+++ Discountable.java
@@ -0,0 +1,15 @@
+interface Discountable{
+
+    //Interfaces, methods are by default public and abstract
+    // so those keywords aren't needed
+    float getMaximumDiscountPCT();
+
+    float getPersonDiscount();
+
+    float getCorporateDiscount();
+
+
+
+
+
+}
